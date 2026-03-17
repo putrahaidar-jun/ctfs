067ae5ab635fb421dd56830cefe5b726ae0def0d
putrahaidar-jun <putrahaidar150@gmail.com>
2026-03-17 13:36:32 +0700

    change app logo

--- src/app/rules/page.tsx
+++ src/app/rules/page.tsx
@@ -117,7 +117,7 @@ export default function RulesPage() {
         </motion.div>
 
         <p className="text-gray-50 dark:text-gray-900">
-          VzJHe1lvdV9IYXZlX0FjY2VwdGVkX0FsbF9SdWxlc19BbmRfRXRoaWNzfQ==
+          VzJHe1lvdV9IYXZlX0FjY2VwdGVkX0FsbF9SdWxlc19BbmRfRXRoaWNzfQ=
         </p>
 
         <div className="w-full max-w-3xl mt-2 flex justify-center">
